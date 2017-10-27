df3296a7f57889850bb557b637dc7b51dcb49f3b
kiyon <kiyonlin@163.com>
2017-10-27 09:51:56 +0800

    迁移js测试到tests文件夹中

--- tests/JavaScript/counter.spec.js
+++ tests/JavaScript/counter.spec.js
@@ -1,5 +1,5 @@
 import { mount } from 'vue-test-utils';
-import Counter from '../js/components/Counter.vue'
+import Counter from '../../resources/assets/js/components/Counter.vue'
 import expect from 'expect';
 
 describe('Counter', () => {
